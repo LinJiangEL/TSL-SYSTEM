70b8aeef9057dbcf093b488b0784fb5f60db3df2
LinJiangEL <1633251707@qq.com>
2024-12-15 11:03:05 +0800

    Update.
    Signed-off-by: LinJiangEL <1633251707@qq.com>

--- tools/Psychology/main.py
+++ tools/Psychology/main.py
@@ -1,8 +1,16 @@
 #  Copyright (c) 2024. L.J.Afres, All rights reserved.
-
+from termcolor import colored
 # textblob：情感分析
+
+"""
+    load [modules]
+    
+"""
+
 
 def run():
     print('-'*36)
     print('|    TSL-SYSTEM Psychology Module    |')
     print('-'*36)
+
+    cmd = input(colored('[Psychology]', color='light_cyan') + ' >> ').strip()

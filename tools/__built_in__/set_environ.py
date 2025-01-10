f62475d25c59f0c559ab0e2bf181076c38defade
LinJiang <linjiangxv101@qq.com>
2025-01-10 19:43:41 +0800

    Fixed some bugs in the past.
    Signed-off-by: LinJiang <linjiangxv101@qq.com>

--- tools/__built_in__/set_environ.py
+++ tools/__built_in__/set_environ.py
@@ -17,12 +17,12 @@ def set_env_variable(name, value):
         home = os.getenv("HOME")
         if os.path.exists(os.path.join(home, '.bashrc')):
             with open(os.path.join(home, '.bashrc'), 'a+') as envir:
-                envir.write(f'\nexport "{name}"={value}')
+                envir.write(f'\nexport {name}={value}')
                 envir.close()
         if os.path.exists(os.path.join(home, '.zshrc')):
             with open(os.path.join(home, '.zshrc'), 'a+') as envir:
-                envir.write(f'\nexport "{name}"={value}')
+                envir.write(f'\nexport {name}={value}')
                 envir.close()
-        os.system(f'export "{name}"={value}')
+        os.system(f'export {name}={value}')
     else:
         raise PlatformError('TSL-SYSTEM can only run on the win32 or linux platform!')
